df25bffe39836c4175981c26d4a6880206a43372
Ihdiannaja <ihdiannaja@sinarmasmining.com>
2024-11-05 11:30:55 +0700

    [ADD] profile url

--- src/features/portfolios/Portfolios.jsx
+++ src/features/portfolios/Portfolios.jsx
@@ -6,7 +6,7 @@ export const Portfolios = () => {
         <Box sx={{ marginTop: "24px" }}>
             <Typography variant="body1" fontWeight={"bold"}>Portfolio</Typography>
             <CardsWithButton 
-                url={"https://ihdiannaja.vercel.app"}
+                url={"https://nanotes.vercel.app"}
                 title={"NaNotes"}
                 description={"An easy-to-use web-based app for basic note management, allowing users to create, edit, and delete notes as needed. Designed to keep note-taking simple and organized."}
                 techStack={"Vite + React JS + Python Flask + PostgreSQL"}
